8cf45217603c8a2e4acda6a2900140c8ee1daa85
Bharat <28brana@gmail.com>
2024-08-06 20:42:56 +0530

    Inital Commit

---
+++ src/pages/auth.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import AuthCard from '../component/authcard';
+
+const Auth: React.FC = () => {
+ 
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <AuthCard/>
+    </div>
+  );
+};
+
+export default Auth;
